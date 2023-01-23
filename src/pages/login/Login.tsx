947ae43d33f79f6d26354d3b8821030ed600c5de
sameh11 <sameh@ranadev.io>
2023-01-23 20:52:59 +0100

    add i18next

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,21 +1,32 @@
+import { useTranslation } from 'react-i18next';
 import Logo from '../../components/logo/Logo';
 import './style.scss';
 
 function Login() {
+  const { t } = useTranslation();
+
   return (
     <div className="login">
       <div className="modal">
         <Logo />
         <form className="form-login">
           <div className="input-group">
-            <input type="text" name="email" placeholder="email" />
+            <input
+              type="text"
+              name="email"
+              placeholder={`${t('login.email')}`}
+            />
             <div className="input-icon" />
           </div>
           <div className="input-group">
-            <input type="password" name="password" placeholder="password" />
+            <input
+              type="password"
+              name="password"
+              placeholder={`${t('login.password')}`}
+            />
             <div className="input-icon" />
           </div>
-          <button type="submit">Log In</button>
+          <button type="submit">{t('login.login')}</button>
         </form>
       </div>
     </div>
