c996855db8a9f6f5ec18812416a14c9e0de8a72a
sameh11 <sameh@ranadev.io>
2023-01-25 21:37:33 +0100

    add props to TomatoTrolleyLogo

--- src/assets/images/TomatoTrolleyLogo.tsx
+++ src/assets/images/TomatoTrolleyLogo.tsx
@@ -2,7 +2,7 @@ import React, { SVGProps } from 'react';
 
 function TomatoTrolleyLogo(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={105} height={30}>
+    <svg xmlns="http://www.w3.org/2000/svg" width={105} height={30} {...props}>
       <path
         style={{
           stroke: 'none',
