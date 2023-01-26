87a005498d42ecb1ea70576a42f06a09c3281045
sameh11 <sameh@ranadev.io>
2023-01-26 19:40:08 +0100

    create client.ts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {
-  ApolloClient,
-  ApolloProvider,
-  createHttpLink,
-  InMemoryCache,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { ApolloProvider } from '@apollo/client';
 import Login from './pages/Login';
-import { getToken } from './utils/auth';
-import IContext from './interfaces';
-import serverURL from './utils/constants';
+import client from './utils/client';
 
 function App() {
-  const httpLink = createHttpLink({
-    uri: serverURL,
-  });
-
-  const authLink = setContext((_, { headers }: IContext): IContext => {
-    // get the authentication token from local storage if it exists
-    const token = getToken();
-    // return the headers to the context so httpLink can read them
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    };
-  });
-
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
-
   return (
     <ApolloProvider client={client}>
       <Router>
