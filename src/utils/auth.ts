5d9528ea0d01e80e91e573ce937e528f7cb3108c
sameh11 <sameh@ranadev.io>
2023-01-26 19:15:23 +0100

    generate hooks folder

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,31 @@
+import decode from 'jwt-decode';
+
+interface IDecodedToken {
+  exp: number;
+  iat: number;
+  userId: string;
+  isAdmin: boolean;
+}
+
 export const authLogin = (token: string) => {
   localStorage.setItem('access-token', token);
 };
 
+const isTokenExpired = (token: string) => {
+  try {
+    const decoded: IDecodedToken = decode(token);
+    if (decoded.exp < Date.now() / 1000) {
+      return true;
+    }
+    return false;
+  } catch (err) {
+    return false;
+  }
+};
+
 export const getToken = () => localStorage.getItem('access-token') || '';
+
+export const loggedIn = () => {
+  const token = getToken();
+  return !!token && !isTokenExpired(token);
+};
