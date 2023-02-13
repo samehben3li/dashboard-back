724573c8bfdfa403e0fca33d997481f2dd145dbd
sameh11 <sameh@ranadev.io>
2023-02-13 22:17:27 +0100

    move InputGroup to separate component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,38 +1,12 @@
-import React, { Dispatch, FormEvent, SetStateAction, useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Logo from '../components/common/Logo';
 import Error from '../components/common/Error';
 import { inputsOfLogin } from '../utils/constants';
 import useLogin from '../hooks/useLogin';
 import Main from '../components/common/Main';
-import { IUser } from '../interfaces';
-import useHandleChange from '../hooks/useHandleChange';
 import Button from '../components/common/Buttons/Button';
-
-interface IPropsInput {
-  type: string;
-  name: string;
-  className: string;
-  placeholder: string;
-  setCredentials: Dispatch<SetStateAction<IUser>>;
-  value: string;
-}
-
-function InputGroup({ setCredentials, placeholder, ...props }: IPropsInput) {
-  const { t } = useTranslation();
-  const handleChange = useHandleChange(setCredentials);
-
-  return (
-    <div className="input-group">
-      <input
-        {...props}
-        onChange={handleChange}
-        placeholder={`${t(placeholder)}`}
-      />
-      <div className="input-icon" />
-    </div>
-  );
-}
+import InputGroup from '../components/common/InputGroup';
 
 function Login() {
   const [credentials, setCredentials] = useState({ email: '', password: '' });
