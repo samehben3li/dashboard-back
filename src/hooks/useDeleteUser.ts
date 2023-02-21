7a9cc63a866f0ab609a2eadd4d7922253a92bc4c
sameh11 <sameh@ranadev.io>
2023-02-21 13:39:35 +0100

    optimize code of useDeleteUser hook

--- src/hooks/useDeleteUser.ts
+++ src/hooks/useDeleteUser.ts
@@ -11,23 +11,24 @@ const useDeleteUser = (
   const [deleteUser, { loading }] = useMutation(DELETE_USER);
   const handleDelete = async (id: string) => {
     setError({ status: false, message: '' });
-    try {
-      await deleteUser({
-        variables: {
-          id,
-        },
-      });
-      setUsers(prev => prev?.filter(u => u.id !== id));
-      setAlertDelete(false);
-    } catch ({ message, ...err }) {
-      setError({
-        status: true,
-        message:
-          message === 'DATA_NOT_FOUND'
-            ? 'USER_NOT_FOUND'
-            : (message as string) || 'SOMETHING_WENT_WRONG',
-      });
-    }
+    deleteUser({
+      variables: {
+        id,
+      },
+    })
+      .then(() => {
+        setUsers(prev => prev?.filter(u => u.id !== id));
+        setAlertDelete(false);
+      })
+      .catch(({ message }) =>
+        setError({
+          status: true,
+          message:
+            message === 'DATA_NOT_FOUND'
+              ? 'USER_NOT_FOUND'
+              : (message as string) || 'SOMETHING_WENT_WRONG',
+        }),
+      );
   };
   return { handleDelete, loading };
 };
